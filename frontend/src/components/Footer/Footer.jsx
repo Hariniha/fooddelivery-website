fa5a903d041bc7a561f7ea11ddd52c0577b2c9aa
Hariniha <harinikannan302004@gmail.com>
2024-08-27 10:24:48 +0530

    first commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./Footer.css"
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id="footer">
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt=''/>
+                <p>dummyGenerate Lorem Ipsum placeholder text for use in your graphic, print and web layouts, and discover plugins for your favorite writing, design and blogging tools. Explore the origins, history and meaning of the famous passage, and learn how Lorem Ipsum went from text jhwdjhiferhuifrygjewhbcjsncuiegfyyer</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">Copyright 2024 @ Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
